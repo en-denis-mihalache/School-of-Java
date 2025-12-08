f2b070dc624a98a04bee0f2cee1f7f9c2fdae07b
Denis Mihalache <georgel-denis.mihalache@endava.com>
2025-12-08 12:08:17 +0200

    Added my progress on the book and the additional research.

--- Spring/TaskEngineHW/src/main/java/repository/EmployeeRepository.java
+++ Spring/TaskEngineHW/src/main/java/repository/EmployeeRepository.java
@@ -11,17 +11,11 @@ public class EmployeeRepository {
     List<Employee> employees = new ArrayList<>(
             List.of(
                     new Employee("Mirel", new ArrayList<>(List.of("Java", "Springboot"))),
-                    new Employee("Denis", new ArrayList<>(List.of("C++", "Android"))),
+                    new Employee("Alina", new ArrayList<>(List.of("C++", "Android"))),
                     new Employee("George", new ArrayList<>(List.of("Java","Kotlin"))),
                     new Employee("Alexandra", new ArrayList<>(List.of("Android","JavaScript")))
             ));
 
-    public Employee findEmployeeByName(String employeeName) {
-        return employees.stream()
-                .filter(e -> e.getName().equals(employeeName))
-                .findFirst()
-                .orElse(null);
-    }
 
     public Employee findEmployeeBySkill(String skill) {
         return employees.stream()
